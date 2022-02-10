f9d746dfe45e1bfc674b3c4f5340a91362edc421
HanSeongJae <fhqhxm8899@gmail.com>
2022-02-11 01:50:11 +0900

    response와 request, router 함수

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 import express from "express";
 
-const app = express();
 const PORT = 4000;
 
+const app = express();
+
+const handleHome = (req, res) =>{
+    console.log("somebody wants to get");
+    return res.send("I still love you");
+}
+
+const handleLogin = (req,res)=>{
+    return res.send("Login here.");
+}
+
+app.get("/",handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on port 4000! http://localhost:${PORT}`);
 
