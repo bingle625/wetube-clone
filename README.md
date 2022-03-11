b1014bd1b10789ff7b1b758350f1f6e00da4d17e
HanSeongJae <fhqhxm8899@gmail.com>
2022-03-12 04:35:08 +0900

    4.6 Planning Routes

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # youtube clone
 
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit My Profile
+/users/delete -> Delete My Profile
+
+/videos/:id -> See video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
+
 ## 목차
 
 1. [4장:ROUTERS](#40-what-is-routers)
--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("watch");
+export const see = (req, res) => res.send("see Video");
 export const edit = (req, res) => res.send("Edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("upload Video");
+export const deleteVideo = (req, res) => res.send("delete Video");
