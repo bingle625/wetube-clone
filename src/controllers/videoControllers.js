280df5ba582668c79bd54535c379e36292e1ee40
HanSeongJae <fhqhxm8899@gmail.com>
2022-03-15 00:04:50 +0900

    5.3 Extending Templates

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,12 +1,6 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`watch video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send(`edit video #${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload Video");
 export const deleteVideo = (req, res) => {
