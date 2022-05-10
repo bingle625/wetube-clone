61d2f2d4aa4f50f798bef79a045bf12e1c6cea60
HanSeongJae <fhqhxm8899@gmail.com>
2022-05-10 20:48:31 +0900

    mongoDB를 이용해서 실제 데이터 db에 업로드, 오류 핸들링

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -11,6 +11,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -35,6 +36,25 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: "Date.now()",
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      meta: {
+        Views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
