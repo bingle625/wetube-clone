776e9e8e92ff882cfd878673bbc458d179fe1b77
HanSeongJae <fhqhxm8899@gmail.com>
2022-05-11 03:14:46 +0900

    FEAT: search 기능추가 및, 기본적인 CRUD 완성

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,16 +1,8 @@
+import { response } from "express";
 import Video from "../models/Video";
 
-// export const home = (req, res) => {
-//   Video.find({}, (err, videos) => {
-//     if (err) {
-//       return res.render("server-error");
-//     }
-//     return res.render("home", { pageTitle: "Home", videos });
-//   });
-// };
-
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -74,4 +66,18 @@ export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+        // $regex: new RegExp(`^${keyword}`, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
