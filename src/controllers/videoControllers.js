1db1b0dbc94e4d630e350a470fc67e73362179fc
HanSeongJae <fhqhxm8899@gmail.com>
2022-05-10 21:43:20 +0900

    home 화면에서 detailed page 연결

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -11,13 +11,14 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  res.render("watch", { pageTitle: `Watching: ` });
+  const video = await Video.findById(id);
+  console.log(video);
+  res.render("watch", { pageTitle: video.title, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
@@ -28,7 +29,6 @@ export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   video.title = title;
-  console.log(req.body);
   return res.redirect(``);
 };
 
@@ -43,12 +43,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      createdAt: "Date.now()",
       hashtags: hashtags.split(",").map((word) => `#${word}`),
-      meta: {
-        Views: 0,
-        rating: 0,
-      },
     });
     return res.redirect("/");
   } catch (error) {
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: { type: Date, required: true },
-  hashtags: [{ type: String }],
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
   meta: {
-    Views: Number,
-    rating: Number,
+    Views: { type: Number, default: 0 },
+    rating: { type: Number, default: 0 },
   },
 });
 
