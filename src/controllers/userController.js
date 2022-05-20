b0ce51a4833d7b51f3654a820602a70167f198a4
HanSeongJae <fhqhxm8899@gmail.com>
2022-05-21 00:37:30 +0900

    로그인을 위해서 계정이 없는지 check

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import * as bcrypt from "bcrypt";
+import { reset } from "nodemon";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -19,17 +20,41 @@ export const postJoin = async (req, res) => {
     });
   }
 
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "join",
+      errorMessage: error._message,
+    });
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res
+      .status(400)
+      .render("login", {
+        pageTitle: "Login",
+        errorMessage: "An account with this username does not exists.",
+      });
+  }
+  // check if account exists
+  // check if password correct
+  reset.end();
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("See User");
